65c05888613f601769e1b62652fa4688307f33fd
Demetrius Veras De Oliveira <demetrius.v@samsung.com>
2019-05-31 14:44:55 -0400

    matching_object_scale function added to obj_detector_matching

--- obj_detector_matching.py
+++ obj_detector_matching.py
@@ -1,8 +1,10 @@
 import argparse
+import glob
 import subprocess
 import sys
 
 import cv2
+import imutils
 import numpy as np
 
 
@@ -34,7 +36,7 @@ def matching_all_objects(img_source, img_template):
     img_rgb = cv2.imread(img_source)
     template = cv2.imread(img_template)
 
-    w, h = template.shape[:-1]
+    w, h = img_rgb.shape[:-1]
 
     method = cv2.TM_CCOEFF_NORMED
 
@@ -43,19 +45,19 @@ def matching_all_objects(img_source, img_template):
     threshold = 0.8
 
     loc = np.where(res >= threshold)
-    print(loc[::-1])
 
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), 255, 4)
+        print(pt)
+        cv2.rectangle(template, pt, (pt[0] + w, pt[1] + h), 255, 4)
 
-    return img_rgb
+    return template
 
 
 def matching_object(img_source, img_template):
     img_rgb = cv2.imread(img_source)
     template = cv2.imread(img_template)
 
-    w, h = template.shape[:-1]
+    w, h = img_rgb.shape[:-1]
 
     method = cv2.TM_CCOEFF_NORMED
 
@@ -65,9 +67,9 @@ def matching_object(img_source, img_template):
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    cv2.rectangle(img_rgb, top_left, bottom_right, 255, 4)
+    cv2.rectangle(template, top_left, bottom_right, 255, 4)
 
-    return img_rgb
+    return template
 
 
 def matching_object_2(img_source, img_template):
@@ -79,6 +81,7 @@ def matching_object_2(img_source, img_template):
     result = cv2.matchTemplate(small_image, large_image, method)
 
     mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+    print(cv2.minMaxLoc(result))
 
     MPx, MPy = mnLoc
 
@@ -88,11 +91,53 @@ def matching_object_2(img_source, img_template):
         large_image,
         (MPx, MPy),
         (MPx+tcols, MPy+trows),
+        (0, 255, 0),
+        4
+    )
+
+    return large_image
+
+
+def matching_object_scale(img_source, img_template):
+    template = cv2.imread(img_template, 0)
+    template = cv2.Canny(template, 50, 200)
+    (tH, tW) = template.shape[:2]
+
+    img_s = cv2.imread(img_source)
+    gray = cv2.cvtColor(img_s, cv2.COLOR_BGR2GRAY)
+    found = None
+
+    for scale in np.linspace(0.2, 1.0, 30)[::-1]:
+        resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+        ratio = gray.shape[1] / float(resized.shape[1])
+
+        # print(resized.shape, ratio)
+        if resized.shape[0] < tH or resized.shape[1] < tW:
+            break
+
+        edge = cv2.Canny(resized, 50, 200)
+        result = cv2.matchTemplate(edge, template, cv2.TM_CCOEFF)
+        (_, max_val, _, max_loc) = cv2.minMaxLoc(result)
+
+        if found is None or max_val > found[0]:
+            found = (max_val, max_loc, ratio)
+
+    (_, max_loc, ratio) = found
+    (start_x, start_y) = (int(max_loc[0] * ratio), int(max_loc[1] * ratio))
+    (end_x, end_y) = (
+        int((max_loc[0] + tW) * ratio),
+        int((max_loc[1] + tH) * ratio)
+    )
+
+    cv2.rectangle(
+        img_s,
+        (start_x, start_y),
+        (end_x, end_y),
         (0, 0, 255),
         2
     )
 
-    return large_image
+    return img_s
 
 
 def main():
@@ -105,7 +150,8 @@ def main():
     source_path = args.source
     template_path = args.template
 
-    result = matching_object_2(source_path, template_path)
+    # result = matching_object(source_path, template_path)
+    result = matching_object_scale(source_path, template_path)
 
     show_window('Output', result)
 
