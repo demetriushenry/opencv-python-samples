d982b1d57dc934c2a6193d76f7a05da62adf164d
Demetrius Veras De Oliveira <demetrius.v@samsung.com>
2019-06-17 14:33:11 -0400

    inserting dlib samples

---
+++ dlib_face_detector.py
@@ -0,0 +1,22 @@
+import cv2
+
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+cam = cv2.VideoCapture(0)
+color_green = (0, 255, 0)
+line_width = 3
+
+while True:
+    ret_val, img = cam.read()
+    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    dets = detector(rgb_image, 1)
+    for det in dets:
+        cv2.rectangle(img, (det.left(), det.top()),
+                      (det.right(), det.bottom()), color_green, line_width)
+    cv2.imshow('DLIB Face Detector', img)
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
+
+cam.release()
+cv2.destroyAllWindows()
