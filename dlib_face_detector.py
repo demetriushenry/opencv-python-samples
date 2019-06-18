10b589e979ae41fab29c22670a84d4809ae089c5
Demetrius Veras De Oliveira <demetrius.v@samsung.com>
2019-06-18 09:21:46 -0400

    face detector dlib updated

--- dlib_face_detector.py
+++ dlib_face_detector.py
@@ -9,8 +9,9 @@ line_width = 3
 
 while True:
     ret_val, img = cam.read()
-    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    dets = detector(rgb_image, 1)
+    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # face detection is faster in gray frame
+    dets = detector(rgb_image, 0)
     for det in dets:
         cv2.rectangle(img, (det.left(), det.top()),
                       (det.right(), det.bottom()), color_green, line_width)
