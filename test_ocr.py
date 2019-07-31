1429b3696a80ce7635b12b167c5c43918b19dc86
Demetrius Veras De Oliveira <demetrius.v@samsung.com>
2019-07-31 14:31:36 -0400

    adjustment

--- test_ocr.py
+++ test_ocr.py
@@ -5,7 +5,8 @@ import pytesseract
 image = cv2.imread('/home/demetrius/Pictures/procel_tag.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-tessdata_config = r'--tessdata-dir "/home/demetrius/Comp/tesseract" --oem 1 --psm 3'
+tessdata_dir = '"/home/demetrius/Comp/tesseract"'
+tessdata_config = f'--tessdata-dir {tessdata_dir} --oem 1 --psm 3'
 text = pytesseract.image_to_string(gray, lang='por', config=tessdata_config)
 file = open('/home/demetrius/Documents/saida.txt', 'w+')
 file.write(text.strip('\n'))
